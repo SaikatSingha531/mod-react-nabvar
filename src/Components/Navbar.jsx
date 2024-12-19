06eff402ad61aa5be400affeb97e47c910f86d85
Saikat Singha <saikatsingha531@gmail.com>
2024-12-19 12:48:56 +0530

    add navbar css file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+// import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <>
-    <h1>this is a simple navbar</h1>
+    <div className="container bg-purple-500 max-w-full h-11 flex items-center justify-between p-5">
+        <div className="logo">
+            Logo
+        </div>
+
+        <div className="search flex gap-3">
+            <input type="text"/>
+            <button>Search</button>
+        </div>
+
+        <div className="menu">
+            <ul className='flex gap-3'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
     </>
   )
 }
